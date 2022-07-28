14d7f9da32e3d92cbc4e8e7c2dc6f2c603f40183
neo-terribile <d.terribile@gmx.de>
2022-07-28 19:56:09 +0200

    test

--- code/main.py
+++ code/main.py
@@ -38,6 +38,20 @@ class Game:
 			pygame.display.update()
 			self.clock.tick(FPS)
 
+	def title(self):
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+					sys.exit()
+
+			
+			self.screen.fill('black')
+
+
+			pygame.display.update()
+			self.clock.tick(FPS)
+
 if __name__ == '__main__':
 	game = Game()
-	game.play()
+	game.game()
