717e3cda88c72de24fc070423f1950335cfb16ad
neo-terribile <d.terribile@gmx.de>
2022-08-13 15:39:27 +0200

    graphics

--- tiled/tilesets/ground.tsx
+++ tiled/tilesets/ground.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.0" name="ground" tilewidth="64" tileheight="64" tilecount="256" columns="16">
- <image source="../../aseprite/png/ground.png" width="1024" height="1024"/>
+<tileset version="1.9" tiledversion="1.9.0" name="ground" tilewidth="64" tileheight="64" tilecount="187" columns="17">
+ <image source="../../aseprite/png/ground.png" width="1140" height="756"/>
 </tileset>
